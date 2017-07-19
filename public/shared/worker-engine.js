ee6a47d66a2e48857e5669e442110ead17f74441
Hernan <hernan.mammana@mercadolibre.com>
2017-07-19 02:48:25 -0300

    var name wrong

--- public/shared/worker-engine.js
+++ public/shared/worker-engine.js
@@ -24,12 +24,12 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  
+
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (self.cacheWhitelist.indexOf(cacheName) === -1) {
+          if (self.CACHE_WHITELIST.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
